fdbabf7fae1ec0bc17ec2b107fc91f5c12e042d1
juiceman1014 <justintran1014@gmail.com>
2023-10-17 15:18:00 -0700

    adjusted files to work according to webpack's needs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import printMe from "./print.js";
 console.log("hello");
+
+
+const element = document.createElement("div");
+const btn = document.createElement("button");
+
+btn.innerHTML = "Click me";
+btn.onclick = printMe;
+
+element.appendChild(btn);
+
+
+
+document.body.appendChild(element);
