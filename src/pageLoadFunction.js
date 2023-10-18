b12f9f0fd0b02d3918354a909007ec926243d3e4
juiceman1014 <justintran1014@gmail.com>
2023-10-17 17:00:51 -0700

    directory buttons added to header

--- src/pageLoadFunction.js
+++ src/pageLoadFunction.js
@@ -1,9 +1,19 @@
 export default function pageLoadFunction() {
     const contentDiv = document.getElementById("content");
 
+    const homeBtn = document.createElement("button");
+    homeBtn.textContent = ("home");
+    const menuBtn = document.createElement("button");
+    menuBtn.textContent = ("menu");
+    const contactBtn = document.createElement("button");
+    contactBtn.textContent = ("contact");
+
     const headerDiv = document.createElement("div");
     headerDiv.id = "header";
     headerDiv.textContent = "Good Eats";
+    headerDiv.append(homeBtn);
+    headerDiv.append(menuBtn);
+    headerDiv.append(contactBtn);
 
     const coverImage = document.createElement("img");
     coverImage.id = "cover-image";
@@ -16,6 +26,8 @@ export default function pageLoadFunction() {
     const footerDiv = document.createElement("div");
     footerDiv.id = "footer";
     footerDiv.textContent = "Made by juiceman1014";
+
+
 
     contentDiv.appendChild(headerDiv);
     contentDiv.appendChild(coverImage);
